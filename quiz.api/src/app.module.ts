d312aac6042208b80e9582b6f2cded8ee7c7bb1d
sonmc <sonmc90@gmail.com>
2022-11-27 15:19:58 +0700

    config

--- quiz.api/src/app.module.ts
+++ quiz.api/src/app.module.ts
@@ -1,7 +1,9 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { join } from 'path';
 import { UserModule } from './user/user.module';
 import { QuestionModule } from './question/question.module';
 import { CategoryModule } from './category/category.module';
@@ -12,10 +14,27 @@ import { StudentModule } from './student/student.module';
 import { StudentClassModule } from './student-class/student-class.module';
 import { StudentAnswerModule } from './student-answer/student-answer.module';
 import { QuestionExamModule } from './question-exam/question-exam.module';
-import config from 'ormconfig';
+import { ConfigurationModule } from './configuration/configuration.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(config),
+  imports:  [  
+  ConfigurationModule,
+  TypeOrmModule.forRootAsync({
+    inject: [ConfigService],
+    useFactory: (config: ConfigService) => {
+      return {
+        type: 'mysql',
+        host: config.get<string>('database.host'),
+        port: config.get<number>('database.port'),
+        username: config.get<string>('database.userName'),
+        password: config.get<string>('database.password'),
+        database: config.get<string>('database.name'),
+        entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+        synchronize: true,
+        logging: ['query', 'error'],
+      };
+    },
+  }),
     UserModule, AuthModule,
     UserModule, QuestionModule,
     CategoryModule, AnswerModule, ExamModule, ClassModule, StudentModule, StudentClassModule, StudentAnswerModule, QuestionExamModule,
---
+++ quiz.api/src/configuration/ormconfig.ts
@@ -0,0 +1,17 @@
+import { DataSource } from 'typeorm';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
+ 
+export const connectSource = new DataSource({
+  host: process.env.DATABASE_HOST,
+  type: 'mysql',
+  port: Number(process.env.DATABASE_PORT),
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  entities: ['src/**/**.entity{.ts,.js}'],
+  migrations: ['src/database/migrations/*.ts'],
+  synchronize: false,
+});
+
+// connectSource.initialize();
